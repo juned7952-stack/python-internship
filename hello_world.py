5b6fd807920a4ec687bdd0ec2352f6b6f2a73de9
CODER Juned <juned7952@gmail.com>
2026-01-15 20:00:38 +0530

    Add files via upload
    I HAVE COMPLETED MY TASKK 1

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+# COMPLETING THE TASK 1 
+
+print("MY NAME IS JUNED KHAN")
+print("I AM A INTERN IN ELEVATE LABS")
+print("TODAYS DATE IS 15TH JANUARY 2026")
+
+# USING VARIABLES TO STORE DATA
+name = "JUNED KHAN"
+date = "15TH JANUARY 2026"
+print("MY NAME IS", name)
+print("TODAYS DATE IS", date)
+
+# ASKING FOR USER INPUT
+user_name = input("PLEASE ENTER YOUR NAME: ")
+user_date = input("PLEASE ENTER TODAY'S DATE: ")
+print("MY NAME IS", user_name)  
+print("TODAYS DATE IS", user_date)
